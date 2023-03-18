2667d6f223575d5a6563af4c095d0258d21a9fa5
Amogh Bharne <bh.amogh12@gmail.com>
2023-03-18 22:02:15 +0530

    added ether

--- server/main.py
+++ server/main.py
@@ -80,5 +80,35 @@ def get_transaction_litecoin(key):
         return "invalid data"
     return "valid"
 
+
+@app.route('/api/ethereum/<string:key>', methods=['GET'])
+def get_transaction_ethereum(key):
+    url = "https://api.blockchair.com/ethereum/dashboards/address/"
+    url = url+key
+    payload = {'key': 'G___mnbXHkLk56C80jkTPzLBqiqgKqGs'}
+    res = requests.get(url, params=payload)
+    addr_data = res.json()
+    data2 = addr_data['data']
+    data3 = data2[key]
+    data4 = data3['address']
+    if data4['transaction_count'] == 0:
+        return "invalid data"
+    return "valid"
+
+@app.route('/api/tether/<string:key>', methods=['GET'])
+def get_transaction_tether(key):
+    url = "https://api.blockchair.com/ethereum/erc-20/0xdac17f958d2ee523a2206206994597c13d831ec7/dashboards/address/"
+    url=url+key
+    payload={'key':'G___mnbXHkLk56C80jkTPzLBqiqgKqGs'}
+    res=requests.get(url,params=payload)
+    addr_data=res.json()
+    data2=addr_data['data']
+    data3=data2[key]
+    data4=data3['address']
+    if data4['transaction_count']==0:
+        return "invalid data"
+    return "valid"
+
+
 if __name__ == "__main__":
     app.run(debug=True)
